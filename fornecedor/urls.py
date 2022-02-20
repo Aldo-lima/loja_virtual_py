ebc074848a451390f8cbd92f0f145b4701ff19bf
Aldo-lima <aldocesar0088@gmail.com>
2022-02-20 10:35:07 -0300

    crud fornecedor

--- fornecedor/urls.py
+++ fornecedor/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import categorias, novaCategoria
+from .views import categorias, novaCategoria, fornecedores, novoFornecedor, fornecedorVer, fornecedorEditar,fornecedorDetete
 
 urlpatterns = [
     path('categoria', categorias, name='home_categorias'),
     path('nova_categoria', novaCategoria, name='nova_categoria'),
+    path('/fornecedores', fornecedores, name='listar_fornecedor'),
+    path('/novo_fornecedor', novoFornecedor, name='fornecedor_create'),
+    path('ver_fornecedor/<int:id>', fornecedorVer, name='fornecedor_ver'),
+    path('editar_fornecedor/<int:id>', fornecedorEditar, name='fornecedor_editar'),
+    path('delete_fornecedores/<int:id>', fornecedorDetete, name='fornecedor_delete'),
 ]
--- fornecedor/views.py
+++ fornecedor/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
-from .models import Categoria
-from  .forms import CotegoriaForm
+from .models import Categoria, Fornecedor
+from  .forms import CotegoriaForm, ForncedorForm
 from django.core.paginator import Paginator
 
-# Create your views here.
+#===============================Categoria====================================
+
+#lista categoria
 def categorias(request):
     categoria_list = Categoria.objects.all()
     paginator = Paginator(categoria_list, 5)
@@ -27,3 +29,69 @@ def novaCategoria(request):
             'form': form,
         }
         return render(request, 'categoria/lista_categoria.html', context)
+
+#===========================================Fornecedor
+def fornecedores(request):
+    search = request.GET.get('search')
+    if search:
+        fornecedores = Fornecedor.objects.filter(nome__icontains=search)
+    else:
+
+        fornecedor_list = Fornecedor.objects.all()
+        paginator = Paginator(fornecedor_list, 5)
+        page = request.GET.get('page')
+        fornecedores = paginator.get_page(page)
+    return render(request, 'fornecedor/list_fornecedor.html', {'fornecedores': fornecedores})
+
+
+def novoFornecedor(request):
+    form = ForncedorForm()
+    if request.method == "POST":
+        form = ForncedorForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('listar_fornecedor')
+    else:
+        context = {
+            'form': form,
+        }
+        return render(request, 'fornecedor/create_fornecedor.html', context)
+
+
+def fornecedorVer(request, id):
+    fornecedor = Fornecedor.objects.get(id=id)
+    return render(request, 'fornecedor/detalhes_fornecedor.html', {'fornecedor':fornecedor})
+
+def fornecedorEditar(request, id):
+    if request.method == "GET":
+        objeto = Fornecedor.objects.filter(id=id).first()
+        if objeto is None:
+            return redirect('listar_fornecedor')
+        form = ForncedorForm(instance=objeto)
+        context = {
+           'form': form,
+        }
+        return render(request, 'fornecedor/edite_fornecedor.html', context)
+    elif request.method == "POST":
+        objeto = Fornecedor.objects.filter(id=id).first()
+        if objeto is None:
+            return redirect(reverse('listar_fornecedor'))
+        form = ForncedorForm(request.POST, instance=objeto)
+        if form.is_valid():
+             form.save()
+             return redirect('listar_fornecedor')
+        else:
+           context = {
+             'form': form,
+
+           }
+           return render(request, 'fornecedor/create_fornecedor.html', context)
+
+
+def fornecedorDetete(request, id):
+    fornecedor = Fornecedor.objects.get(id=id)
+    if request.method == "POST":
+        fornecedor.delete()
+        return redirect('listar_fornecedor')
+    else:
+        return render(request, 'fornecedor/delete_confirme.html', {'fornecedor': fornecedor})
